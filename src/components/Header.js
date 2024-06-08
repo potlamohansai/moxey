6c899e0fa900ad9068a3dbaa38d2fa022feb31fd
Mohan Sai Potla <potlamohansai@gmail.com>
2024-06-08 14:51:57 +0530

    Header component created

---
+++ src/components/Header.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <nav class="navbar bg-body-tertiary">
+        <div class="container-flex">
+          <span class="navbar-brand mb-0 h1">
+            Rudra Transporter - Transporter
+          </span>
+        </div>
+        <div class="container-flex">
+          <span class="nav-item mb-0 h6">01/05/2024 - 30/05/2024</span>
+          <span class="nav-item mb-0 h6"> United States</span>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Header;
